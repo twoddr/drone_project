2b8325022a7bcac618e4c1fd3ee3b9ea9607aae0
MK <florentmarc6@gmail.com>
2024-01-24 21:33:05 +0100

    Clean

--- pilotage_ESC.ino
+++ pilotage_ESC.ino
@@ -1,40 +1,34 @@
 #include <Servo.h>
 
-Servo esc;   // Création de l'objet permettant le contrôle de l'ESC
-
-int val = 0; //
-int pot_val;
+Servo esc;  // Create an object to control the ESC
+int val = 0;  // Variable to store the potentiometer value
+int pot_val;  // Variable to store the previous potentiometer value
 
 void setup() {
-   esc.attach(9); // On attache l'ESC au port numérique 9 (port PWM obligatoire)
-   delay(15);
-   Serial.begin(9600);
+  esc.attach(9);  // Attach the ESC to digital pin 9 (PWM pin required)
+  delay(15);
+  Serial.begin(9600);
 
-   // Initialisation de l'ESC
-   //  (certains ESC ont besoin d'une "procédure d'initialisation"
-   //   pour devenir opérationnels - voir notice)
-   esc.write(0);
-   delay(1000);
-   esc.write(1023);
-   delay(1000);
-   esc.write(0);
-
-
-   // Quelques informations pour l'utilisateur
-   /*Serial.println("Saisir un nombre entre 0 et 179");
+  // Initialize the ESC
+  // Some ESCs need an "initialization procedure" to become operational - see manual
+  esc.write(0);
+  delay(1000);
+  esc.write(1023);
+  delay(1000);
+  esc.write(0);
+  /*Serial.println("Saisir un nombre entre 0 et 179");
    Serial.println("(0 = arret - 179 = vitesse maxi");
    Serial.println(" demarrage a partir de 20)");*/
-   }
+}
 
 void loop() {
-    pot_val=analogRead(A0);
-   if (pot_val > 0) {
-      //val = Serial.parseInt();   // lecture de la valeur passée par le port série
-        if (val != pot_val){
-          Serial.println("Valeur analogique : "+val);
-        }
-        val = pot_val;
-        esc.write(val);            //
-        delay(15);
-      }
-   }
+  pot_val = analogRead(A0);  // Read the value from the potentiometer
+  if (pot_val > 0) {
+    if (val != pot_val) {
+        Serial.println("Analog value: " + String(val));  // Print the value if it has changed
+    }
+    val = pot_val;  // Update the stored value
+    esc.write(val);  // Send the value to the ESC
+    delay(15);
+  }
+}
